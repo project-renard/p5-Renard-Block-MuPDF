066d5e83148fc95c824abcd07f5e5703eba762f8
Zakariyya Mughal <zaki.mughal@gmail.com>
2019-12-04 01:08:15 -0600

    Initial skeleton

---
+++ lib/Renard/Block/MuPDF.pm
@@ -0,0 +1,12 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Block::MuPDF;
+# ABSTRACT: MuPDF rendering library block
+
+use Mu;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Block-MuPDF/>
+
+=cut
